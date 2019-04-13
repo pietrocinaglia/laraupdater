347bb456e8e7be46dded6f2ac6235457dbe5971e
Sylvain Roh <syzin12@gmail.com>
2019-04-13 15:30:52 +0200

    Add french translations

---
+++ lang/fr/laraupdater.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lara Updater Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'LaraUpdater' => 'LaraUpdater',
+    'Return_to_App_HOME' => 'Retour à l\'accueil',
+    'ACTION_NOT_ALLOWED.' => 'Action non autorisée',
+    'Your_System_IS_ALREADY_UPDATED_to_last version' => 'Votre système est déjà à jour',
+    'UPDATE_FOUND' => 'Une nouvelle mise à jour est disponible',
+    'current_version' => "version acuelle",
+    'DESCRIPTION' => 'Description',
+    'Update_downloading_..' => 'Mise à jour, téléchargement en cours ...........',
+    'Error_during_download.' => 'Erreur lors du téléchargement.',
+    'OK' => 'Ok',
+    'SYSTEM_Mantence_Mode' => 'Mode maintenance',
+    'ON' => 'Activé',
+    'OFF' => 'Désactivé',
+    'SYSTEM_IS_NOW_UPDATED_TO_VERSION' => 'Le système a été mis a jour en version',
+    'ERROR_DURING_UPDATE_(!!check_the_update_archive!!)' => 'Erreur lors de la mise à jour (!!Veuillez vérifier le fichier de mise à jour!!) --Essayer de restaurer l\'ancienne version ........... ',
+    'CHANGELOG' => 'Changelog',
+    'Directory' => 'Dossier',
+    'File' => 'Fichier',
+    'Commands_successfully_executed.' => 'Commande executée avec succès',
+    'Error_during_commands_execution.' => ' Erreur lors de l\'execution des commandes',
+    'Could_not_save_new_update' => 'Impossible d\'enregister la nouvelle mise à jour (Veuillez vérifier les droits d\'accès du dossier temporaire). Mise à jour annulée.',
+    'FAILED' => 'Echoué',
+    'Backup_folder_is_located_in:' => 'La sauvegarde se trouve dans le dossier:',
+    'Remember_to_restore_System_UP-Status_through_shell_command:' => 'N\'oubliez pas de désactiver le mode de maintenance en utilisant la commande:',
+    'RESTORED' => 'Restauré',
+    'Update_Now' => 'Mettre à jour maintenant',
+    'Update_Available' => 'Une mise à jour est disponible',
+    'Updated' => 'Mis à jour',
+    'Updating' => 'Mise à jour en cours',
+    'error_try_again' => 'Erreur. Veuillez essayer encore.'
+];
