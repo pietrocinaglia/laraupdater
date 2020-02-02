a4c0523ac4b51bdca74e4ea7eff4e12605ecd05d
John <juaorok@hotmail.com>
2020-02-02 18:10:33 -0300

    Create laraupdater.php
    Add Portuguese Brazilian translation

---
+++ lang/pt-br/laraupdater.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lara Updater Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'LaraUpdater' => 'LaraUpdater',
+    'Return_to_App_HOME' => 'Voltar para HOME',
+    'ACTION_NOT_ALLOWED.' => 'AÇÃO NÃO PERMITIDA',
+    'Your_System_IS_ALREADY_UPDATED_to_last version' => 'O seu sistema JÁ ESTÁ ATUALIZADO para a versão mais recente',
+    'UPDATE_FOUND' => 'ATUALIZAÇÃO ENCONTRADA',
+    'current_version' => 'Versão Atual',
+    'DESCRIPTION' => 'Descrição',
+    'Update_downloading_..' => 'Baixando atualização...........',
+    'Error_during_download.' => 'Erro durante o download.',
+    'OK' => 'OK',
+    'SYSTEM_Mantence_Mode' => 'Modo de Manutenção do SISTEMA',
+    'ON' => 'LIGADO',
+    'OFF' => 'DESLIGADO',
+    'SYSTEM_IS_NOW_UPDATED_TO_VERSION' => 'O SISTEMA ESTÁ ATUALIZADO PARA A VERSÃO',
+    'ERROR_DURING_UPDATE_(!!check_the_update_archive!!)' => 'ERRO DURANTE A ATUALIZAÇÃO (!! verifique o arquivo de atualização !!) --TRY para restaurar o status ANTERIOR........... ',
+    'CHANGELOG' => 'REGISTRO DE ALTERAÇÕES',
+    'Directory' => 'Diretório',
+    'File' => 'Arquivo',
+    'Commands_successfully_executed.' => 'Comandos executados com sucesso.',
+    'Error_during_commands_execution.' => 'Erro durante a execução dos comandos.',
+    'Could_not_save_new_update' => 'Não foi possível salvar a nova atualização (verifique a permissão de escrita /tmp). Atualização cancelada.',
+    'FAILED' => 'FALHOU',
+    'Backup_folder_is_located_in:' => 'A pasta de backup está localizada em:',
+    'Remember_to_restore_System_UP-Status_through_shell_command:' => 'Lembre-se de restaurar o status UP do sistema através do comando shell:',
+    'RESTORED' => 'RESTAURADO',
+    'Update_Now' => 'Atualizar agora',
+    'Update_Available' => 'Atualização disponível',
+    'Updated' => 'Atualizado',
+    'Updating' => 'Atualizando',
+    'error_try_again' => 'Erro.Tente novamente'
+];
