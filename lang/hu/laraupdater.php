fbf5bced714fc5f46a83e2aec866cd25d180d869
MDavid <2527132+MDylan@users.noreply.github.com>
2022-02-26 12:54:49 +0100

    Hungarian translation

---
+++ lang/hu/laraupdater.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lara Updater Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'LaraUpdater' => 'LaraUpdater',
+    'Return_to_App_HOME' => 'Vissza a Főoldalra ',
+    'ACTION_NOT_ALLOWED.' => 'AZ ESEMÉNY NEM ENGEDÉLYEZETT',
+    'Your_System_IS_ALREADY_UPDATED_to_last version' => 'A program MÁR FRISSÜLT a legutóbbi változatra',
+    'UPDATE_FOUND' => 'FRISSÍTÉST TALÁLTAM',
+    'current_version' => "jelenlegi verzió",
+    'DESCRIPTION' => 'LEÍRÁS',
+    'Update_downloading_..' => 'Frissítés, letöltés ...........',
+    'Error_during_download.' => 'Hiba a letöltés közben.',
+    'OK' => 'OK',
+    'SYSTEM_Mantence_Mode' => 'RENDSZER Karbantartás Mód',
+    'ON' => 'BE',
+    'OFF' => 'KI',
+    'SYSTEM_IS_NOW_UPDATED_TO_VERSION' => 'A RENDSZER MOST MÁR A LEGFRISSEBB VÁLTOZATTAL FUT',
+    'ERROR_DURING_UPDATE_(!!check_the_update_archive!!)' => 'HIBA A FRISSÍTÉS KÖZBEN (!!ellenőrizd a frissítési fájlt!!) --MEGPRÓBÁLOM visszaállítani a RÉGI állapotot ........... ',
+    'CHANGELOG' => 'VÁLTOZÁSI NAPLÓ',
+    'Directory' => 'Könyvtár',
+    'File' => 'Fájl',
+    'Commands_successfully_executed.' => 'A parancsok sikeresen lefutottak.',
+    'Error_during_commands_execution.' => ' Hiba a parancsok futtatása során.',
+    'Could_not_save_new_update' => 'Nem tudom elmenteni az új frissítést (ellenőrizd a tmp/ könyvtár írási jogát). Frissítés megszakítva.',
+    'FAILED' => 'HIBA',
+    'Backup_folder_is_located_in:' => 'A mentés könyvtára megtalálható itt:',
+    'Remember_to_restore_System_UP-Status_through_shell_command:' => 'Ne felejtsd el visszaállítani a rendszert az UP paranccsal a parancssorban:',
+    'RESTORED' => 'VISSZAÁLLÍTVA',
+    'Update_Now' => 'Frissítés Most',
+    'Update_Available' => 'Frissítés érhető el!',
+    'Updated' => 'Frissítve',
+    'Updating' => 'Frissítés',
+    'error_try_again' => 'Hiba. Próbáld újra'
+];
