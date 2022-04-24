d34b6d716988b9c9099e435617e5deb919669023
Mohammad <sop7ana.allah@gmail.com>
2022-04-24 21:54:17 +0200

    Add Arabic translation

---
+++ lang/ar/laraupdater.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lara Updater Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'LaraUpdater' => 'LaraUpdater',
+    'Return_to_App_HOME' => 'العودة للصفحة الرئيسية ',
+    'ACTION_NOT_ALLOWED.' => 'الإجراء غير مصرّح به',
+    'Your_System_IS_ALREADY_UPDATED_to_last version' => 'النظام محدث بالفعل لآخر إصدار',
+    'UPDATE_FOUND' => 'تم العثور على تحديث',
+    'current_version' => "الإصدار الحالي",
+    'DESCRIPTION' => 'الوصف',
+    'Update_downloading_..' => 'جارٍ تحميل التحديث ........',
+    'Error_during_download.' => 'حدث خطأ أثناء التحميل.',
+    'OK' => 'موافق',
+    'SYSTEM_Mantence_Mode' => 'وضع صيانة النظام',
+    'ON' => 'مفعل',
+    'OFF' => 'مقفل',
+    'SYSTEM_IS_NOW_UPDATED_TO_VERSION' => 'تم تحديث النظام للإصدار',
+    'ERROR_DURING_UPDATE_(!!check_the_update_archive!!)' => 'خطأ أثناء التحديث (!! افحص ملف التحديث المضغوط !!) -- حاول استعادة الحالة القديمة .......',
+    'CHANGELOG' => 'سجل التغييرات',
+    'Directory' => 'المجلد',
+    'File' => 'ملف',
+    'Commands_successfully_executed.' => 'تم تنفيذ الأوامر بنجاح.',
+    'Error_during_commands_execution.' => ' حدث خطأ أثناء تنفيذ الأوامر.',
+    'Could_not_save_new_update' => 'لم نستطع حفظ ملف التحديث (افحص تصارح الكتابة لمجلد tmp/). تم إلغاء التحديث.',
+    'FAILED' => 'فشل',
+    'Backup_folder_is_located_in:' => 'ملف النسخة الإحتياطية محفوظ في:',
+    'Remember_to_restore_System_UP-Status_through_shell_command:' => 'تذكر استعادة حالة تشغيل النظام عبر سطر الأوامر:',
+    'RESTORED' => 'تمت الإسستعادة',
+    'Update_Now' => 'تحديث الآن',
+    'Update_Available' => 'يتوفر تحديث للنظام ',
+    'Updated' => 'تم التحديث',
+    'Updating' => 'جارٍ التحديث',
+    'error_try_again' => 'خطأ. حاول مرة أخرى'
+];
