cb1e258f75e58a4e58ebf18f018c6a917327491b
Luis Pereida <luis.pereida@sysadminctl.services>
2025-02-07 00:00:44 -0600

    Adding ES languague

---
+++ lang/es/laraupdater.php
@@ -0,0 +1,35 @@
+<?php
+/*
+* @author: Luis Pereida
+* https://github.com/pereidax86
+*/
+    /*
+    |--------------------------------------------------------------------------
+    | Lara Updater Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    return [
+        'SYSTEM_VERSION' => 'Versión del Sistema: ',
+        'PERMISSION_DENIED' => 'Este usuario no está autorizado para realizar actualizaciones.',
+        'ALREADY_UPDATED' => 'El sistema está ACTUALIZADO a la ultima versión.',
+        'BACKUP' => 'Carpeta de respaldo creada.',
+        'BACKUP_FOUND' => 'Respaldo Encontrado: ',
+        'DOWNLOADING' => 'Descargando la actualización...',
+        'DOWNLOADING_SUCCESS' => 'Actualización descargada exitosamente.',
+        'DOWNLOADING_ERROR' => 'Ocurrió un error durante la descarga.',
+        'MAINTENANCE_MODE_ON' => 'Modo de mantenimiento: SI',
+        'MAINTENANCE_MODE_OFF' => 'Modo de mantenimiento: NO',
+        'INSTALLATION_SUCCESS' => 'Actualización instalada exitosamente.',
+        'INSTALLATION_ERROR' => 'Ocurrió un error durante la instalación..',
+        'RECOVERY' => 'LaraUpdater esta intentando recuperar su sistema desde un respaldo.',
+        'RECOVERY_SUCCESS' => 'Recuperación exitosa.',
+        'RECOVERY_ERROR' => 'Recuperación fallida, Intente manualmente. Recuerde: \'php artisan up\' para deshabilitar el modo de mantenimiento.',
+        'EXCEPTION' => 'Ha ocurrido una excepción: ',
+        'CHANGELOG' => 'Registro de cambios:',
+        'DIRECTORY_CREATED' => 'Directorio creado: ',
+        'FILE_EXIST' => 'Archivo existente: ',
+        'FILE_COPIED' => 'Archivo copiado: ',
+        'EXECUTE_UPDATE_SCRIPT' => 'Ejecutar el script incluido en la actualización.',
+        'TEMP_CLEANED' => 'Archivos temporales eliminados.',
+    ];
